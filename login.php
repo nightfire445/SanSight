cb33a049fbfd3a85ca7ad11fdd27c1ab098176b5
Solomon <Moris@rpi.edu>
2016-12-03 11:14:11 -0500

    Make sure pass and username is set before checking
    Fixes an error where username and pass are not set (when navigating to
    login or index)

--- login.php
+++ login.php
@@ -23,10 +23,10 @@
   }
 
   
-  if($_POST['username'] == "" || $_POST['password'] == ""){
+  if(isset($_POST['password']) &&  $_POST['password'] == "" || isset($_POST['username']) && $_POST['username'] == "" ){
      $msg = "Username and password must not be left blank.";
 
-  }
+}
 
   if(isset($_POST['register']) && $_POST['username'] != "" && $_POST['password'] != ""){
     echo "registering";
@@ -41,7 +41,7 @@
     
   }
 
-  if (isset($_POST['login']) || isset($_POST['register']) && isset($_POST['password']) ){
+  if (isset($_POST['login']) || isset($_POST['register']) && isset($_POST['password'])  && isset($_POST['username']) ){
     //Obtain User's Salt
     $select_salt = $dbconn->prepare("SELECT salt FROM users WHERE username = :username");
     $select_salt->execute(array(':username' => $_POST['username']));
